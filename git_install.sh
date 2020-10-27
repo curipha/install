193e7e5caa71c8d490d5918d60f4d560c3c6aa59
Taichi <taichi@curipha.jp>
2020-10-27 22:11:52 +0900

    v2.29.1

--- git_install.sh
+++ git_install.sh
@@ -6,7 +6,7 @@ set -o nounset
 set -o errexit
 
 APPLICATION=Git
-VERSION=2.28.0
+VERSION=2.29.1
 CONFIGURE_OPT=( USE_LIBPCRE1=1 NO_EXPAT=1 NO_TCLTK=1 NO_GETTEXT=1 ) # Change to USE_LIBPCRE2 if you want to use PCRE v2
 
 URIFORMAT='https://www.kernel.org/pub/software/scm/git/git-${VERSION}.tar.xz'
